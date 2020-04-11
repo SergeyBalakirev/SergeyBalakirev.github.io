a3045b849aa381cc38f1efad9d89bc030afe1914
SergeyBalakirev <60481214+SergeyBalakirev@users.noreply.github.com>
2020-04-11 06:49:41 +0300

    Actistatin

---
+++ project_1/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows: false,
+		
+  	autoplay: true,
+  	autoplaySpeed: 300000,
+  	infinite: true,
+  	speed: 700,
+  	// fade: true,
+  	cssEase: 'linear'
+	});
+});
+
+
+
+
+
