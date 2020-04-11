9502f8235f18cce99b458cb39fd2a61ed5344c14
SergeyBalakirev <60481214+SergeyBalakirev@users.noreply.github.com>
2020-04-11 06:42:32 +0300

    WayUp

---
+++ project_6/js/main.js
@@ -0,0 +1,76 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.clock').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.travelling').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.value').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+
+		$(window).scroll(function() {
+	    $('.name').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+	    $('.email').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+	    $('.phone').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+	    $('.location').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+})
