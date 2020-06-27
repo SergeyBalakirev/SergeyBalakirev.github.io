df60a22e337c21d70cf3bf3f714a2d941d38a945
SergeyBalakirev <60481214+SergeyBalakirev@users.noreply.github.com>
2020-06-27 13:09:44 +0300

    mr.barber

---
+++ mr.barber/js/main.js
@@ -0,0 +1,73 @@
+$(function () {
+
+  $('.look-book__inner').slick({
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
+
+  $('.testimonials__slider-inner').slick({
+    prevArrow: '<button class="slick-arrow slick-prev icon-lnr-arrow-left"></button>',
+    nextArrow: '<button class="slick-arrow slick-next icon-lnr-arrow-right"></button>',
+
+  });
+
+  $('.news__slider-inner').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  // $('.burger').on('click', function () {
+  //   $('.burger').toggleClass('burger__active');
+
+  //   $('.header__menu').slideToggle();
+  // });
+
+  // $('.header__menu-link').on('click', function () {
+  //   $('.header__menu').slideToggle();
+
+  //   $('.burger').toggleClass('burger__active');
+  // });
+
+
+
+
+  if (window.matchMedia('(max-width: 720px)').matches) {
+    $('.burger').on('click', function () {
+      $('.burger').toggleClass('burger__active');
+
+      $('.header__menu').slideToggle();
+    });
+
+    $('.header__menu-link').on('click', function () {
+      $('.header__menu').slideToggle();
+
+      $('.burger').toggleClass('burger__active');
+    });
+  }
+
+
+});
